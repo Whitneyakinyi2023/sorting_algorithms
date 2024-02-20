afb8d730eb97a269f8ee5db108f65e3d76e0916a
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-02-20 04:51:31 -0800

    betty compliant

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,30 +9,29 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t a, b;
-        /* Outer loop iterates for each pass through the array*/
-        for (a = 0; a < size - 1; a++)
-        {
-                bool swapped = false;
-                /* Flag to track if any swaps occur*/
+	size_t a, b;
+	/* Outer loop iterates for each pass through the array*/
+	for (a = 0; a < size - 1; a++)
+	{
+		bool swapped = false;
+		/* Flag to track if any swaps occur*/
+		/*Inner loop compares adjacent elements*/
+	for (b = 0; b < size - a - 1; b++)
+	{
+		if (array[b] > array[b + 1])
+		{
+			/* Swap elements in the wrong order*/
+			int temp = array[b];
 
-        /*Inner loop compares adjacent elements*/
-        for (b = 0; b < size - a - 1; b++)
-        {
-                if (array[b] > array[b + 1])
-                {
-                        /* Swap elements in the wrong order*/
-                        int temp = array[b];
-                        array[b] = array[b + 1];
-                        array[b + 1] = temp;
-                        swapped = true;
-                }
-        }
-
-        /* Optimization: If no swaps occurr*/
-        if (!swapped)
-        {
-                break;
-        }
-    }
+			array[b] = array[b + 1];
+			array[b + 1] = temp;
+			swapped = true;
+		}
+	}
+	/* Optimization: If no swaps occurr*/
+	if (!swapped)
+	{
+		break;
+	}
+	}
 }
