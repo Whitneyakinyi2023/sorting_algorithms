ecb4bc97484708657917f8c1406612e0b1f6c91b
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-02-20 04:21:46 -0800

    repo plus files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "sort.h"
+/**
+ * bubble_sort - function that sorts an array using bubble sort
+ * @array: Array
+ * @size: size
+ */
+void bubble_sort(int *array, size_t size)
+{
+        size_t a, b;
+        /* Outer loop iterates for each pass through the array*/
+        for (a = 0; a < size - 1; a++)
+        {
+                bool swapped = false;
+                /* Flag to track if any swaps occur*/
+
+        /*Inner loop compares adjacent elements*/
+        for (b = 0; b < size - a - 1; b++)
+        {
+                if (array[b] > array[b + 1])
+                {
+                        /* Swap elements in the wrong order*/
+                        int temp = array[b];
+                        array[b] = array[b + 1];
+                        array[b + 1] = temp;
+                        swapped = true;
+                }
+        }
+
+        /* Optimization: If no swaps occurr*/
+        if (!swapped)
+        {
+                break;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+This project explores various sorting algorithms in C and analyzes their time complexity using Big O notation.
+
+Goals:
+
+Understand the concept of Big O notation and its significance in algorithm analysis.
+Implement different sorting algorithms in C (e.g., Bubble Sort, Insertion Sort, Selection Sort, Merge Sort, Quick Sort).
+Analyze the time complexity of each implemented algorithm using Big O notation.
+Compare the performance of different sorting algorithms based on their time complexity.
